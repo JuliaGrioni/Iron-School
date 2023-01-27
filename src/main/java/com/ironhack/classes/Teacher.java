96594f7dcadf92c90ba07ed4fe9ff248caeb1e9c
dmitri <dmitrisidorov@hotmail.com>
2023-01-27 12:43:20 +0100

    first commit

---
+++ src/main/java/com/ironhack/classes/Teacher.java
@@ -0,0 +1,37 @@
+package com.ironhack.classes;
+
+public class Teacher {
+    private String teacherId;
+    private String name;
+    private double salary;
+
+    public Teacher(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+        teacherId = String.valueOf(teacherId.hashCode());
+    }
+
+    public String getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(String teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
